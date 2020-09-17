71b8b539e88c7b025c482ea21542278580f1bc21
Vadim Sviridov <Vadim Sviridov>
2020-09-17 13:29:09 +0300

    delete src1

--- src1/redux/actions/user-management/userEditActions.js
+++
@@ -1,6 +0,0 @@
-export const setEditUser = (user) => {
-    return {
-        type: "SET_EDIT_USER",
-        payload: { ...user }
-    }
-};
--- src1/redux/reducers/user/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux"
-import { profile, list } from "./userReducer"
-
-const userReducers = combineReducers({
-    profile: profile,
-    list: list
-})
-
-export default userReducers
