b9c7422aa74e0aa6697176a96e928dd47b9987bb
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2020-11-20 22:42:23 +0200

    Add capitalize utility

---
+++ src/utility/common.js
@@ -0,0 +1,7 @@
+export function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function capitalizeAll(string) {
+  return string.split(" ").map((word) => capitalizeFirstLetter(word)).join(" ")
+}
