dd6d76fb107c97a872264df196e9275bc06a26e5
alexey.b <alexey.b@resty-app.com>
2020-12-02 13:21:13 +0200

    updates breadcrumbs ms

--- src/views/pages/master-schema/index.js
+++ src/views/pages/master-schema/index.js
@@ -1,9 +1,21 @@
 import React, {useState, useEffect} from 'react';
-
-import {Card, CardBody, CardHeader, CardTitle, Row, Col, Button, Table} from 'reactstrap'
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Row,
+  Col,
+  Button,
+  Table,
+  UncontrolledButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap'
 import masterSchemaService from './services/masterSchema.service'
 import Select from "react-select"
-import {X, Plus} from "react-feather";
+import {X, Plus, ChevronDown } from "react-feather";
 import rfdc from "rfdc";
 import FieldEdit from "./FieldEdit";
 import GroupEdit from "./GroupEdit";
@@ -43,7 +55,7 @@ function MasterSchema() {
 
   useEffect(() => {
     setMasterSchemaTreebeard(null);
-    setCursor(null);
+    closeElement();
     getCurrentMasterSchema();
   }, [organization]);
 
@@ -61,7 +73,15 @@ function MasterSchema() {
     }
   };
 
-  const recursiveMap = (node) => {
+  const closeElement = () => {
+    if (cursor) {
+      cursor.active = false;
+      setMasterSchemaTreebeard(Object.assign({}, masterSchemaTreebeard));
+      setCursor(null);
+    }
+  };
+
+  const recursiveMap = (node, path = []) => {
 
     if (!node) return null;
 
@@ -69,8 +89,8 @@ function MasterSchema() {
     node.children = node.fields;
     node.toggled = true;
 
-    if(cursor) {
-      if(
+    if (cursor) {
+      if (
         node.id === cursor.id && cursor.children && node.children
       ) {
         node.active = true;
@@ -79,8 +99,14 @@ function MasterSchema() {
     }
 
     node.children.forEach((child) => {
-      if(cursor) {
-        if(
+      let nodePath = path.slice();
+      nodePath.push(child.name);
+      child.path = nodePath;
+
+
+      // set previous cursor
+      if (cursor) {
+        if (
           child.id === cursor.id && !cursor.children && !child.children
         ) {
           child.active = true;
@@ -92,7 +118,10 @@ function MasterSchema() {
     if (node.groups.length) {
 
       for (let group of node.groups) {
-        group = recursiveMap(group);
+        let nodePath = path.slice();
+        nodePath.push(group.name);
+        group.path = nodePath;
+        group = recursiveMap(group, nodePath);
       }
       node.children = node.children.concat(node.groups);
     }
@@ -100,7 +129,9 @@ function MasterSchema() {
     return node;
   };
   const parseToFormatTreebeard = () => {
-    const root = recursiveMap(clone(masterSchema.root));
+    const rootPath = [masterSchema.root.name];
+    const root = recursiveMap(clone(masterSchema.root), rootPath);
+    root.path = rootPath;
     setMasterSchemaTreebeard(root);
   };
 
@@ -122,13 +153,12 @@ function MasterSchema() {
 
 
   const onToggle = (node, toggled) => {
-    if(cursor) {
+    if (cursor) {
       cursor.active = false;
       masterSchemaTreebeard.active = false;
     }
     node.active = true;
 
-    console.log(node, masterSchemaTreebeard);
     setCursor(node);
     setMasterSchemaTreebeard(Object.assign({}, masterSchemaTreebeard))
   };
@@ -139,13 +169,13 @@ function MasterSchema() {
 
   const outputTreeColumn = (node, data = []) => {
     data.push(node);
-    if(node.children) {
+    if (node.children) {
       node.children.forEach(child => outputTreeColumn(child, data))
     }
     return data;
   };
 
- return (
+  return (
     <Row>
       <Col md="6">
         <Card>
@@ -202,22 +232,25 @@ function MasterSchema() {
                       <td className="w-50">
                         {
                           outputTreeColumn(masterSchemaTreebeard).map(element => <div className="ms-tree-column">
-                            <Tabs className="w-100" onChange={() => {}} tabs={[element.name, element.name]}></Tabs>
+                            <Tabs className="w-100" onChange={() => {
+                            }} tabs={[element.name, element.name]}></Tabs>
                           </div>)
                         }
                       </td>
                     </tr>
                     </tbody>
                   </Table>
-
-                  <Button
-                    onClick={() => {
-                    }}
-                    color="primary"
-                    className="add-icon btn-add-ms-element p-0"
-                  >
-                    <Plus size={28}/>
-                  </Button>
+                  <div className="dropright mr-1 mb-1 d-inline-block">
+                    <UncontrolledButtonDropdown direction="right">
+                      <DropdownToggle color="primary" className="add-icon btn-add-ms-element" caret>
+                        <Plus size={28}/>
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem tag="a">Category</DropdownItem>
+                        <DropdownItem tag="a">Element</DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledButtonDropdown>
+                  </div>
                 </div>
             }
           </CardBody>
@@ -227,14 +260,16 @@ function MasterSchema() {
         {
           !cursor ? null : <Card>
             <CardHeader>
-              <CardTitle>{cursor.name}</CardTitle>
-              <X size={15} className="cursor-pointer mr-1" onClick={event => setCursor(null)}/>
+              <CardTitle>
+                <Breadcrumbs list={cursor.path}/>
+              </CardTitle>
+              <X size={15} className="cursor-pointer mr-1" onClick={event => closeElement()}/>
             </CardHeader>
             <CardBody>
               {
                 'children' in cursor ?
                   <GroupEdit data={cursor} onChange={(group) => {
-                    setCursor(recursiveMap(group));
+                    // setCursor(recursiveMap(group));
                     getCurrentMasterSchema();
                   }} onNewField={(newField) => {
                     getCurrentMasterSchema();
