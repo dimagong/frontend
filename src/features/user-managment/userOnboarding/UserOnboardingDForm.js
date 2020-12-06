5f769965d729921936b711ceac66bd621d047c1f
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2020-12-07 01:08:40 +0200

    Fix dform styles

--- src/features/user-managment/userOnboarding/UserOnboardingDForm.js
+++ src/features/user-managment/userOnboarding/UserOnboardingDForm.js
@@ -76,7 +76,7 @@ const UserOnboardingDForm = () => {
   };
   return (
     <Col md="12" className="mb-4">
-      <Card className="border">
+      <Card className="dform border">
         <CardHeader className="m-0">
           <CardTitle>
             Onboarding dForm
