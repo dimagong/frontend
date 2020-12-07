28cdc369a2782883093d97d3fe78c3b3018d049c
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2020-12-07 21:50:45 +0200

    Remove master schema data from user profile. Clean user profile from unused stuff

--- src/features/user-managment/userEdit/UserEdit.js
+++ src/features/user-managment/userEdit/UserEdit.js
@@ -1,47 +1,24 @@
 import React, {useState, useRef, useEffect} from 'react'
 import {
   Card,
-  CardHeader,
-  CardTitle,
+
   CardBody,
-  FormGroup,
+
   Row,
   Col,
-  Input,
-  Form,
-  FormFeedback,
-  Media,
-  Nav,
-  NavItem,
-  NavLink,
+
   TabPane,
   Button,
   TabContent,
-  Pagination,
-  PaginationItem,
-  PaginationLink,
-  UncontrolledButtonDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  Table,
+
 } from "reactstrap"
 import DataTable from "react-data-table-component"
-import Flatpickr from "react-flatpickr";
-import Select, {components} from "react-select"
-import classnames from "classnames"
-import moment from 'moment';
-import {toast} from "react-toastify"
-import {User, X, Check, Plus, Edit2, RefreshCw, EyeOfxf, Eye, ChevronRight, ChevronLeft} from "react-feather"
-import {colourStyles} from "utility/select/selectSettigns";
-import {DropdownIndicator} from 'components/MultiSelect/multiSelect'
-// import InvitationCreate from '../invitation/InvitationCreate'
+
+import { Plus } from "react-feather"
+
 import { useDispatch, useSelector } from "react-redux";
 import {
-  selectGroups,
-  selectRoles,
   selectManager,
-  selectManagers,
   selectModules,
   selectUserDForms,
   selectUserWorkfows, selectUserReviewers
@@ -55,10 +32,7 @@ import {
   setManagerOnboarding, getUserOnboardingRequest
 } from "app/slices/appSlice";
 import {useOutsideAlerter} from 'hooks/useOutsideAlerter'
-import UserEditAvatar from "./UserEditAvatar"
-import UserEditSelects from './UserEditSelects';
-import UserOnboarding from '../userOnboarding/UserOnboarding';
-import UserInvitationsCreate from '../userInvitations/UserInvitationsCreate';
+
 import {columnDefs} from '../userOnboarding/gridSettings'
 import UserOnboardingForm from '../userOnboarding/UserOnboardingForm'
 import UserOnboardingDForm from '../userOnboarding/UserOnboardingDForm'
@@ -68,16 +42,6 @@ import CustomTabs from 'components/Tabs'
 import Timeline from 'components/Timeline'
 import UserRoles from 'components/UserRoles'
 import {selectUserOrganizations} from 'app/selectors/userSelectors'
-import masterSchemaService from '../../../views/pages/master-schema/services/masterSchema.service'
-import Breadcrumbs from '../../../views/pages/master-schema/Breadcrumbs'
-import MasterSchemaTree from '../../../views/pages/master-schema/MasterSchemaTree/MasterSchemaTree'
-import GroupEdit from '../../../views/pages/master-schema/GroupEdit'
-import FieldEdit from '../../../views/pages/master-schema/FieldEdit'
-
-import Tabs from '../../../components/Tabs/index.js'
-import rfdc from 'rfdc'
-
-const clone = rfdc();
 
 const UserEdit = (props, context) => {
 
@@ -156,157 +120,6 @@ const UserEdit = (props, context) => {
   const formSubmit =() => {
       // dispatch(createUserOnboardingRequset())
   }
-
-
-  // Master schema data =================================================================================================================
-  const [masterSchemaIsLoading, setMasterSchemaIsLoading] = useState(false);
-  const [organization, setOrganization] = useState();
-  const [masterSchema, setMasterSchema] = useState();
-  const [masterSchemaTreebeard, setMasterSchemaTreebeard] = useState();
-  const [organizations, setOrganizations] = useState([]);
-  const [cursor, setCursor] = useState(false);
-
-
-  const getOrganizations = async () => {
-    const response = await masterSchemaService.getOrganizations();
-    const organizationsByType = response.data.data;
-    let organizations = []
-      .concat(organizationsByType.corporation)
-      .concat(organizationsByType.network)
-      .concat(organizationsByType.member_firm);
-    setOrganizations(organizations);
-  };
-
-  useEffect(() => {
-    getOrganizations();
-  }, []);
-
-  useEffect(() => {
-    setMasterSchemaTreebeard(null);
-    closeElement();
-    getCurrentMasterSchema();
-  }, [organization]);
-
-  useEffect(() => {
-    if (masterSchema) {
-      parseToFormatTreebeard();
-    }
-
-  }, [masterSchema]);
-
-
-  const getCurrentMasterSchema = () => {
-    if (organization && organization.value) {
-      getMasterSchemaByType(organization.value.type, organization.value.id);
-    }
-  };
-
-  const closeElement = () => {
-    if (cursor) {
-      cursor.active = false;
-      setMasterSchemaTreebeard(Object.assign({}, masterSchemaTreebeard));
-      setCursor(null);
-    }
-  };
-
-  const recursiveMap = (node, path = []) => {
-
-    if (!node) return null;
-
-    node.children = [];
-    node.children = node.fields;
-    node.toggled = true;
-
-    if (cursor) {
-      if (
-        node.id === cursor.id && cursor.children && node.children
-      ) {
-        node.active = true;
-        setCursor(node);
-      }
-    }
-
-    node.children.forEach((child) => {
-      let nodePath = path.slice();
-      nodePath.push(child.name);
-      child.path = nodePath;
-
-
-      // set previous cursor
-      if (cursor) {
-        if (
-          child.id === cursor.id && !cursor.children && !child.children
-        ) {
-          child.active = true;
-          setCursor(child);
-        }
-      }
-    });
-
-    if (node.groups.length) {
-
-      for (let group of node.groups) {
-        let nodePath = path.slice();
-        nodePath.push(group.name);
-        group.path = nodePath;
-        group = recursiveMap(group, nodePath);
-      }
-      node.children = node.children.concat(node.groups);
-    }
-
-    return node;
-  };
-  const parseToFormatTreebeard = () => {
-    const rootPath = [masterSchema.root.name];
-    const root = recursiveMap(clone(masterSchema.root), rootPath);
-    root.path = rootPath;
-    setMasterSchemaTreebeard(root);
-  };
-
-  const createMasterSchema = async () => {
-    const response = await masterSchemaService.create(organization.value.type, organization.value.id);
-    setMasterSchema(response.data.data);
-  };
-
-  const getMasterSchemaByType = async (type, id) => {
-    try {
-      const response = await masterSchemaService.getByOrganization(type, id);
-      setMasterSchema(response.data.data);
-    } catch (exception) {
-      console.log(exception);
-    } finally {
-      setMasterSchemaIsLoading(false)
-    }
-  };
-
-
-  const onToggle = (node, toggled) => {
-    if (cursor) {
-      cursor.active = false;
-      masterSchemaTreebeard.active = false;
-    }
-    node.active = true;
-
-    setCursor(node);
-    setMasterSchemaTreebeard(Object.assign({}, masterSchemaTreebeard))
-  };
-
-  const isNeedToCreateMS = () => {
-    return !masterSchema && organization;
-  };
-
-  const outputTreeColumn = (node, data = []) => {
-    data.push(node);
-    if (node.children) {
-      node.children.forEach(child => outputTreeColumn(child, data))
-    }
-    return data;
-  };
-
-  // ======================================================================================================================================
-
-
-
 
     return (
       <Row className="user-managment">
