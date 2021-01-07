9800a9cf12e83ae645c92e2d386798f6d29f886e
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2021-01-07 22:35:27 +0200

    Fix styles for user invite form

--- src/features/invitation/Invitation.js
+++ src/features/invitation/Invitation.js
@@ -66,7 +66,7 @@ const Invitation = () => {
           <Card className="bg-authentication login-card rounded-0 mb-0 w-100">
             <Row className="m-0">
               <Col lg="12" md="12" className="p-0">
-                <Card className="rounded-0 mb-0 px-2 login-tabs-container">
+                <Card className="rounded-0 mb-0 px-2">
                   <CardHeader className="pb-1">
                     <CardTitle>
                       <h4 className="mb-0">Invitation request form</h4>
@@ -87,7 +87,7 @@ const Invitation = () => {
                           {...{invalid: errors['password']}}
                         />
                         <Label>Password</Label>
-                        <FormFeedback>Oh noes! that name is already taken</FormFeedback>
+                        <FormFeedback>{errors['password']}</FormFeedback>
                       </FormGroup>
                       <FormGroup className="form-label-group position-relative">
                         <Input
@@ -100,7 +100,7 @@ const Invitation = () => {
                         />
 
                         <Label>Password confirmation</Label>
-                        <FormFeedback>Oh noes! that name is already taken</FormFeedback>
+                        <FormFeedback>{errors['password_confirmation']}</FormFeedback>
                       </FormGroup>
                       <div className="d-flex justify-content-end">
                         <Button color="primary" type="submit" onClick={onSubmit}>
