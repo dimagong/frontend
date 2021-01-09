f36b146325eaafb26a4a3cdfc4d0f8927904f066
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2021-01-09 03:03:38 +0200

    Revert changes

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,8 +1,7 @@
 export const userService = {
   isOnboarding(userProfile) {
 
-    // Fixing token issue when redux state isAuth = true and no token set
-    // if(isEmpty(userProfile)) return true;
+    if(isEmpty(userProfile)) return true;
 
     return userProfile?.permissions?.ability === 'prospect'
   }
