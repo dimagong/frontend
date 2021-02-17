27fb40a08e72527b340579eb12676838dbfed8c2
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2021-02-17 21:05:50 +0200

    Refactor: move user management from CS to external component

---
+++ src/features/home/ContextSearch/UserManagement/index.js
@@ -0,0 +1,130 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  ChevronLeft,
+  ChevronRight,
+  X,
+} from 'react-feather'
+import UserEditPreview from 'features/user-managment/userEdit/userEditPreview'
+
+import {
+  Row,
+  Col,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Button,
+} from 'reactstrap'
+import { setManager, setPreview } from 'app/slices/appSlice'
+import useWindowSize from 'hooks/windowWidth'
+import UserCardTemplate from '../CardTemplates/userCard'
+import { selectPreview } from 'app/selectors/layoutSelector'
+
+const UserManagement = ({ managers, handleContextChange }) => {
+
+  const dispatch = useDispatch()
+  const preview = useSelector(selectPreview)
+  const [page, setPage] = useState(0);
+
+  const closePreview = () => {
+    dispatch(setPreview(null))
+    setPage(0)
+  }
+
+  const width = useWindowSize()
+
+  let itemsPerPage = width <= 1400 ? 6 : 9;
+
+  itemsPerPage = preview ? 3 : itemsPerPage;
+  const oneColumn = !!preview
+
+  const renderContent = (data, page) => {
+
+    const pageData = data.slice(itemsPerPage * page, itemsPerPage * (page + 1))
+
+    return pageData.map((elData) => (
+      <UserCardTemplate
+        className="cursor-pointer"
+        oneColumn={oneColumn}
+        onClick={(e, user) => {
+          if (e.ctrlKey) {
+            dispatch(setPreview({type: "user", id: user.id}))
+          } else {
+            dispatch(setManager(user));
+            handleContextChange("User")
+          }
+        }}
+        {...elData}
+      />
+    ))
+  }
+
+  const getPagination = () => {
+    return Array.from(Array(Math.ceil(managers.length/itemsPerPage)))
+  }
+
+  const onPreviewExpand = () => {
+    const previewedManager = managers.filter(({ id }) => id === preview.id)[0]
+    dispatch(setManager(previewedManager))
+    handleContextChange("User")
+  }
+
+
+
+  return (
+    <Row>
+      <Col className={`home__card-wrapper ${preview ? "preview-visible" : ""}`} sm={preview ? oneColumn ? "5" : "8" : "12"}>
+        {managers && renderContent(managers, page)}
+        {managers && getPagination().length > 1 && (
+          <div className="search-context-pagination">
+            <Button
+              className="pagination-arrow"
+              onClick={() => {
+                if (page !== 0) setPage(page - 1)
+              }}
+            >
+              <ChevronLeft size={28} color="#707070"/>
+            </Button>
+            <Pagination aria-label="Page navigation">
+              {getPagination().map( (_, index) => (
+                <PaginationItem key={index} active={page === index}>
+                  <PaginationLink onClick={() => {
+                    setPage(index)
+                  }}>
+                    {index + 1}
+                  </PaginationLink>
+                </PaginationItem>
+              ))}
+            </Pagination>
+            <Button
+              className="pagination-arrow"
+              onClick={() => {
+                if (page !== getPagination().length -1) setPage(page + 1)
+              }}
+            >
+              <ChevronRight size={28} color="#707070"/>
+            </Button>
+          </div>
+        )}
+      </Col>
+      {preview && (
+        <Col sm={oneColumn ? "7" : "4"} className="preview">
+
+          <UserEditPreview />
+
+          <div className="preview-action-buttons">
+            <Button style={{borderRadius: "5rem"}} onClick={onPreviewExpand}>
+              Expand
+            </Button>
+            <Button style={{borderRadius: "5rem", padding: "10px"}} onClick={closePreview}>
+              <X size={20}/>
+            </Button>
+          </div>
+        </Col>
+      )}
+    </Row>
+  )
+}
+
+export default UserManagement
+
--- src/features/home/ContextSearch/index.js
+++ src/features/home/ContextSearch/index.js
@@ -1,23 +1,14 @@
 import React, { useEffect, useState } from "react";
 import {
-  Card,
-  CardBody,
   Row,
   Col,
-  Pagination,
-  PaginationItem,
-  PaginationLink,
-  Badge,
   Button,
   TabContent,
   TabPane,
 } from "reactstrap";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  selectdForms, selectGroups,
-  selectManager,
   selectManagers,
-  selectNotifications,
   selectContext, selectProfile
 } from "app/selectors";
 
@@ -26,67 +17,39 @@ import {
   getdFormsRequest,
   getNotificationsRequest,
   getWorkflowsRequest,
-  setManager,
   setContext,
-  setPreview,
 } from "app/slices/appSlice";
 
 import {
-  setWorkflow,
-  setdForm,
-  setNotification,
-} from 'app/slices/onboardingSlice'
-
-import { X, ChevronLeft, ChevronRight, ChevronUp, Plus } from 'react-feather'
+  ChevronUp,
+  Plus,
+} from 'react-feather'
 
 import { NAV_OPTIONS } from './ContextSearchNav/constants'
 
 import ContextSearchNav from './ContextSearchNav'
 
-import {selectLoading} from 'app/selectors'
-import {selectWorkflows} from 'app/selectors/onboardingSelectors'
 import { selectAuth } from "app/selectors/authSelectors"
 import {selectVuexyUser} from 'app/selectors'
-import {selectPreview} from '../../../app/selectors/layoutSelector'
-
-import DFormCardTemplate from './CardTemplates/dFormCard'
-import WorkFlowTemplate from './CardTemplates/workFlowCard'
-import UserCardTemplate from './CardTemplates/userCard'
-import NotificationTemplate from './CardTemplates/notificationsCard'
-import useWindowSize from '../../../hooks/windowWidth'
-
-import UserEditPreview from 'features/user-managment/userEdit/userEditPreview'
-import DFormFormPreview from 'features/onboarding/dForm/DFormFormPreview'
-import WorkflowFormPreview from 'features/onboarding/workflow/workflowPreview'
-import NotificationsFormPreview from 'features/onboarding/notifications/notificationPreview'
-
 import Applications from './Applications'
 import MasterSchema from 'views/pages/master-schema'
 import Organizations from './Organizations'
+import UserManagement from './UserManagement'
 
 import './styles.scss'
-import {getGroupName} from '../../../utility/select/prepareSelectData'
-import {groupTypes} from '../../../constants/group'
 
 const ContextSearch = ({isShown, onContextSearchHide}) => {
   const dispatch = useDispatch();
 
-  const manager = useSelector(selectManager);
   const managers = useSelector(selectManagers);
-  const dForms = useSelector(selectdForms)
-  const notifications = useSelector(selectNotifications)
-  const workFlows = useSelector(selectWorkflows)
-  const isLoading = useSelector(selectLoading)
+
   const isAuth = useSelector(selectAuth)
   const vuexyUser = useSelector(selectVuexyUser)
-  const preview = useSelector(selectPreview)
-  const groups = useSelector(selectGroups)
   const context = useSelector(selectContext)
   const profile = useSelector(selectProfile)
 
-  const [page, setPage] = useState(0);
   const [selectedNavItem, setSelectedNavItem] = useState(NAV_OPTIONS[0])
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
 
   const handleContextChange = (context) => {
     dispatch(setContext(context))
@@ -102,92 +65,14 @@ const ContextSearch = ({isShown, onContextSearchHide}) => {
     }
   }
 
-  const closePreview = () => {
-    dispatch(setPreview(null))
-    setPage(0)
-  }
-
-  const width = useWindowSize()
-  let itemsPerPage = width <= 1400 ? 6 : 9;
-
-  itemsPerPage = preview ? 3 : itemsPerPage;
-  const oneColumn = !!preview
-
-  // Slice data depending on page, decide which template to use and render it
-  const renderContent = (data, type, page) => {
-
-    const sliceData = (data, page) => {
-
-
-      return data.slice(itemsPerPage * page, itemsPerPage * (page + 1))
-    }
-    const getOrganizationName = (groupId, groupType) => {
-      return getGroupName(groups, groupId, groupTypes[groupType])
-    }
-
-    const templates = {
-      dForms: <DFormCardTemplate oneColumn={oneColumn} onClick={(e, dForm) => {
-                if (e.ctrlKey) {
-                  dispatch(setPreview({type: "dForm", id: dForm.id}))
-                } else {
-                  dispatch(setdForm(dForm));
-                  handleContextChange("dForm")
-                }
-              }} />,
-      managers: <UserCardTemplate className="cursor-pointer" oneColumn={oneColumn} onClick={(e, user) => {
-                  if (e.ctrlKey) {
-                    dispatch(setPreview({type: "user", id: user.id}))
-                  } else {
-                    dispatch(setManager(user));
-                    handleContextChange("User")
-                  }
-                }} />,
-      workFlows: <WorkFlowTemplate oneColumn={oneColumn} onClick={(e, workFlow) => {
-                  if (e.ctrlKey) {
-                    dispatch(setPreview({type: "workFlow", id: workFlow.id}))
-                  } else {
-                    dispatch(setWorkflow(workFlow))
-                    handleContextChange("WorkFlow")
-                  }
-                }} />,
-      notifications: <NotificationTemplate oneColumn={oneColumn} onClick={(e, notification) => {
-                    if (e.ctrlKey) {
-                      dispatch(setPreview({type: "notification", id: notification.id}))
-                    } else {
-                      dispatch(setNotification(notification))
-                      handleContextChange("Notification")
-                    }
-                    }} />,
-    }
-    return sliceData(data, page).map((elData) => (React.cloneElement(templates[type], {...elData, key: elData.id})))
-  }
-
-
   const handleNavItemChange = (navItem) => {
     setSelectedNavItem(navItem)
-  }
-
-  const getPagination = () => {
-    return Array.from(Array(Math.ceil(data[selectedNavItem.id].length/itemsPerPage)))
-  }
-
-  const onPreviewExpand = () => {
-    const previewedManager = managers.filter(({ id }) => id === preview.id)[0]
-    dispatch(setManager(previewedManager))
-    handleContextChange("User")
   }
 
   const handleContextSearchHide = () => {
     if (context) {
       onContextSearchHide()
     }
-  }
-
-  const data = {
-    dForms,
-    notifications,
-    workFlows,
-    managers,
   }
 
   useEffect(() => {
@@ -198,15 +83,6 @@ const ContextSearch = ({isShown, onContextSearchHide}) => {
       dispatch(getNotificationsRequest())
     }
   }, [isAuth, vuexyUser]);
-
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-  }, [])
-
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth)
-  }
 
   let nav;
 
@@ -239,60 +115,7 @@ const ContextSearch = ({isShown, onContextSearchHide}) => {
                         <Col>
                           <TabContent activeTab={selectedNavItem.id}>
                             <TabPane tabId={NAV_OPTIONS[0].id}>
-                              <Row>
-                                <Col className={`home__card-wrapper ${preview ? "preview-visible" : ""}`} sm={preview ? oneColumn ? "5" : "8" : "12"}>
-                                  {data[selectedNavItem.id] && renderContent(data[selectedNavItem.id], selectedNavItem.id, page)}
-                                  {data[selectedNavItem.id] && getPagination().length > 1 && (
-                                    <div className="search-context-pagination">
-                                      <Button
-                                        className="pagination-arrow"
-                                        onClick={() => {
-                                          if (page !== 0) setPage(page - 1)
-                                        }}
-                                      >
-                                        <ChevronLeft size={28} color="#707070"/>
-                                      </Button>
-                                      <Pagination aria-label="Page navigation">
-                                        {getPagination().map( (_, index) => (
-                                          <PaginationItem key={index} active={page === index}>
-                                            <PaginationLink onClick={() => {
-                                              setPage(index)
-                                            }}>
-                                              {index + 1}
-                                            </PaginationLink>
-                                          </PaginationItem>
-                                        ))}
-                                      </Pagination>
-                                      <Button
-                                        className="pagination-arrow"
-                                        onClick={() => {
-                                          if (page !== getPagination().length -1) setPage(page + 1)
-                                        }}
-                                      >
-                                        <ChevronRight size={28} color="#707070"/>
-                                      </Button>
-                                    </div>
-                                  )}
-                                </Col>
-                                {preview && (
-                                  <Col sm={oneColumn ? "7" : "4"} className="preview">
-                                    {{
-                                      user: <UserEditPreview />,
-                                      dForm: <DFormFormPreview />,
-                                      workFlow: <WorkflowFormPreview />,
-                                      notification: <NotificationsFormPreview />,
-                                    }[preview.type]}
-                                    <div className="preview-action-buttons">
-                                      <Button style={{borderRadius: "5rem"}} onClick={onPreviewExpand}>
-                                        Expand
-                                      </Button>
-                                      <Button style={{borderRadius: "5rem", padding: "10px"}} onClick={closePreview}>
-                                        <X size={20}/>
-                                      </Button>
-                                    </div>
-                                  </Col>
-                                )}
-                              </Row>
+                              <UserManagement managers={managers} handleContextChange={handleContextChange} />
                             </TabPane>
                             <TabPane tabId={NAV_OPTIONS[1].id}>
                               <Applications />
@@ -303,7 +126,6 @@ const ContextSearch = ({isShown, onContextSearchHide}) => {
                             <TabPane tabId={NAV_OPTIONS[3].id}>
                               <Organizations />
                             </TabPane>
-
                           </TabContent>
 
                         </Col>
