e67fa3b8bf480b94f800e5d3a94e444c1e256f0f
hryhorchuk117 <hryhorchuk117@gmail.com>
2021-04-08 20:28:12 +0300

    userReducers rollbacked

--- src/app/reducers/app/user/userReducers.js
+++ src/app/reducers/app/user/userReducers.js
@@ -307,17 +307,8 @@ const allowUserAbilitySuccess = (state, {payload}) => {
   const editedOrg = user.organizations[payload.data.organization_type].filter(({id}) => id === payload.data.organization_id)[0];
   editedOrg.abilities = payload.response;
 
-  if (state.user.managers[userIndex]?.permissions?.organization === editedOrg.name) {
+  if (state.user.managers[userIndex].permissions.organization === editedOrg.name) {
     user.permissions.ability = payload.data.ability
-  } else if (!state.user.managers[userIndex].permissions) {
-    user.permissions = {
-      organization: editedOrg.name,
-      organization_id: payload.data.organization_id,
-      organization_type: payload.data.organization_type,
-      ability: payload.data.ability,
-      logo_path: editedOrg.logo_path,
-      logo: editedOrg.logo
-    }
   }
 
   state.user.managers[userIndex] = user;
@@ -327,11 +318,7 @@ const allowUserAbilitySuccess = (state, {payload}) => {
 const disallowUserAbilitySuccess = (state, {payload}) => {
   const userIndex = state.user.managers.findIndex((manager) => manager.id === payload.data.user_id);
   const user = state.user.managers[userIndex];
-  const editedOrg = user.organizations[payload.data.organization_type].filter(({id}) => id === payload.data.organization_id)[0];
-  editedOrg.abilities = payload.response;
-  if (state.user.managers[userIndex]?.permissions?.organization === editedOrg.name) {
-    user.permissions = null;
-  }
+  user.organizations[payload.data.organization_type].filter(({id}) => id === payload.data.organization_id)[0].abilities = payload;
   state.user.managers[userIndex] = user;
   state.isLoading = false;
 };
