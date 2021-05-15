553fe0f8af5dc90fade538f831bb61f1b94546e9
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2021-05-15 05:14:35 +0300

    Implement new survey form components

---
+++ src/features/Surveys/Components/SurveyFormComponents/index.js
@@ -0,0 +1,11 @@
+import Input from './Input'
+import TextArea from './TextArea';
+import Checkbox from "./Checkbox";
+import Select from './Select';
+
+export {
+  Input,
+  TextArea,
+  Checkbox,
+  Select,
+}
