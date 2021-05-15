06f31bb94802ba6a3d4353b5e01eb20d376276ab
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2021-05-15 05:10:55 +0300

    Implement surveys saga

---
+++ src/app/saga/surveys/index.js
@@ -0,0 +1,103 @@
+import { all, put, call, takeLatest } from "redux-saga/effects";
+
+import surveysApi from "api/surveys";
+
+import appSlice from 'app/slices/appSlice';
+
+const {
+  getSurveysSuccess,
+  getSurveysRequest,
+  getSurveysError,
+
+  createSurveySuccess,
+  createSurveyRequest,
+  createSurveyError,
+
+  createFolderSuccess,
+  createFolderRequest,
+  createFolderError,
+
+  getFoldersSuccess,
+  getFoldersRequest,
+  getFoldersError,
+
+  createQuestionSuccess,
+  createQuestionRequest,
+  createQuestionError,
+
+  updateQuestionSuccess,
+  updateQuestionRequest,
+  updateQuestionError,
+}  = appSlice.actions;
+
+function* getSurveys() {
+  const response = yield call(surveysApi.getSurveys);
+
+  if (response?.message) {
+    yield put(getSurveysError(response.message))
+  } else {
+    yield put(getSurveysSuccess(response))
+  }
+}
+
+function* createSurvey(payload) {
+  const response = yield call(surveysApi.createSurvey, payload);
+
+  if (response?.message) {
+    yield put(createSurveyError(response.message))
+  } else {
+    yield put(createSurveySuccess(response))
+  }
+}
+
+function* createFolder(payload) {
+  const response = yield call(surveysApi.createFolder, payload);
+
+  if (response?.message) {
+    yield put(createFolderError(response.message))
+  } else {
+    yield put(createFolderSuccess(response))
+  }
+}
+
+function* getFolders() {
+  const response = yield call(surveysApi.getFolders);
+
+  if(response?.message) {
+    yield put(getFoldersError(response.message))
+  } else {
+    yield put(getFoldersSuccess(response))
+  }
+}
+
+function* createQuestion(payload) {
+  const response = yield call(surveysApi.createQuestion, payload);
+
+  if(response?.message) {
+    yield put(createQuestionError(response.message))
+  } else {
+    yield put(createQuestionSuccess({response, folderId: payload.payload.folder_id}))
+  }
+}
+
+function* updateQuestion(payload) {
+  console.log("update question payload", payload);
+  const response = yield call(surveysApi.updateQuestion, payload);
+
+  if(response?.message) {
+    yield put(updateQuestionError(response.message))
+  } else {
+    yield put(updateQuestionSuccess({response, folderId: payload.payload.data.folder_id}))
+  }
+}
+
+export default function* () {
+  yield all([
+    yield takeLatest(getSurveysRequest.type, getSurveys),
+    yield takeLatest(createSurveyRequest.type, createSurvey),
+    yield takeLatest(createFolderRequest.type, createFolder),
+    yield takeLatest(getFoldersRequest.type, getFolders),
+    yield takeLatest(createQuestionRequest.type, createQuestion),
+    yield takeLatest(updateQuestionRequest.type, updateQuestion),
+  ]);
+}
