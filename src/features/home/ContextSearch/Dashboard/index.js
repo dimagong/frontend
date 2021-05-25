d96acce8c31af012b451f7b2a2b94febced17c6d
hryhorchuk117 <hryhorchuk117@gmail.com>
2021-05-25 15:36:47 +0300

    dashboard design updated

--- src/features/home/ContextSearch/Dashboard/index.js
+++ src/features/home/ContextSearch/Dashboard/index.js
@@ -8,6 +8,7 @@ import ActivitiesDashboard from "./ActivitiesDashboard";
 import ArrowDown from "assets/img/svg/arrow_down.svg";
 import ArrowLeft from "assets/img/svg/arrow_left.svg";
 import ArrowRight from "assets/img/svg/arrow_right.svg";
+import CloseChart from "assets/img/svg/closeChart.svg";
 
 const {
   getDashboardDataRequest,
@@ -18,70 +19,48 @@ const Dashboard = ({ }) => {
   const dispatch = useDispatch();
   const dashboardData = useSelector(selectDashboardData)
   const [isActivitiesShown, setIsActivitiesShown] = useState(true);
+  const [isChartShown, setIsChartShown] = useState(true);
   const [isOneChartShown, setIsOneChartShown] = useState(true);
-  const [daysNumber, setDaysNumber] = useState([7, 7])
+  const [daysNumber, setDaysNumber] = useState(7)
 
   useEffect(() => {
     dispatch(getDashboardDataRequest({page: 1}));
     dispatch(getActivityTypesRequest());
   }, []);
 
+  if (!isChartShown && !isActivitiesShown) {
+    return null
+  }
+
   return (<div className={'activity-dashboard'}>
     <h1 style={{fontWeight: 'bolder', color: '#707070'}}>Dashboard</h1>
-    <div className={'dashboard-charts'}>
-      {isOneChartShown
-        ? <div className={'dashboard-one-chart'}>
-            <span className={'arrow-left'} onClick={() => setIsOneChartShown(false)}>
-              <img src={ArrowLeft}/>
+    {isChartShown && <div className={'dashboard-charts'}>
+      <div style={isOneChartShown ? {width: '45%'} : {width: '22.5%'}} className={'dashboard-one-chart'}>
+            <span className={'arrow-left'}>
+              <img src={isOneChartShown ? ArrowLeft : ArrowRight} onClick={() => setIsOneChartShown(!isOneChartShown)}/>
+              <img src={CloseChart} className={'close-chart'} onClick={() => setIsChartShown(false)}/>
             </span>
-            <span className={'change-chart-days'}>
+            <span className={'change-chart-days ' + (!isOneChartShown ? ' change-chart-days-smaller' : '')}>
               {[{label: 'y', daysNumber: 365}, {label: 'm', daysNumber: 28}, {label: 'w', daysNumber: 7}].map(item => {
-                return <span onClick={() => setDaysNumber([item.daysNumber, daysNumber[1]])}
-                             className={'chart-days ' + (daysNumber[0] === item.daysNumber ? 'active-days' : '')}>
+                return <span onClick={() => setDaysNumber(item.daysNumber)}
+                             className={'chart-days ' + (daysNumber === item.daysNumber ? 'active-days' : '')}>
                   {item.label}
                 </span>
               })}
             </span>
+            {!isActivitiesShown && isOneChartShown &&
+              <span className={'arrow-open-activities'} onClick={() => setIsActivitiesShown(true)}>
+                <img src={ArrowDown}/>
+              </span>}
             <LineChart
-              chartId={'one-chart'}
+              chartId={'chart-1'}
               data={dashboardData?.usersActivitiesSchedule}
-              aspectRatio={4}
-              daysNumber={daysNumber[0]}
+              aspectRatio={isOneChartShown ? 3 : 1.5}
+              daysNumber={daysNumber}
             />
-          </div>
-        : <div>
-            {[0, 1].map(item =>
-              <div style={item === 0 ? {marginRight: '2%'} : {}} className={'dashboard-chart-parts'}>
-                <span className={'arrow-left'}
-                      onClick={() => {setDaysNumber([daysNumber[item], daysNumber[(item + 1) % 2]]); setIsOneChartShown(true)}}>
-                  <img src={ArrowRight}/>
-                </span>
-                <span className={'change-chart-days multiple-change-chart-days'}>
-                  {[{label: 'y', daysNumber: 365}, {label: 'm', daysNumber: 28}, {label: 'w', daysNumber: 7}].map(itemDays => {
-                    return <span onClick={() => {
-                              let copyArr = daysNumber.slice();
-                              copyArr[item] = itemDays.daysNumber;
-                              setDaysNumber(copyArr)}}
-                                 className={'chart-days ' + (daysNumber[item] === itemDays.daysNumber ? 'active-days' : '')}>
-                      {itemDays.label}
-                    </span>
-                  })}
-                </span>
-                <LineChart
-                  chartId={'chart-' + item.toString()}
-                  data={dashboardData?.usersActivitiesSchedule}
-                  aspectRatio={2}
-                  daysNumber={daysNumber[item]}
-                />
-              </div>)}
-          </div>
-
-      }
-      {!isActivitiesShown &&
-      <span className={'arrow-open-activities'} onClick={() => setIsActivitiesShown(true)}>
-        <img src={ArrowDown}/>
-      </span>}
-    </div>
+      </div>
+    </div>}
+    {isActivitiesShown &&
     <div className={'dashboard-activities'}>
       <h3 className={'users-activities-title'}>Users activities</h3>
       <ActivitiesDashboard
@@ -89,7 +68,7 @@ const Dashboard = ({ }) => {
         isActivitiesShown={isActivitiesShown}
         setIsActivitiesShown={setIsActivitiesShown}
       />
-    </div>
+    </div>}
   </div>)
 }
 
