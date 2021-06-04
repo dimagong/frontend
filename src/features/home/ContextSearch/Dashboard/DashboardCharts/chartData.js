ef3f78343de8da83735e4c30d2aee2bf009f0fa5
hryhorchuk117 <hryhorchuk117@gmail.com>
2021-06-04 15:29:36 +0300

    FIX: showing only first application chart bug

--- src/features/home/ContextSearch/Dashboard/DashboardCharts/chartData.js
+++ src/features/home/ContextSearch/Dashboard/DashboardCharts/chartData.js
@@ -69,22 +69,22 @@ const dataApplicationChart = ({data, daysNumber, title, isSmall}) => {
     return null;
   }
   let pointsY = {'in-progress': [],  'submitted': [], 'approved': [], "rejected": [] };
-  let currDForm = undefined;
+  //let currDForm = undefined;
   for (let i = 1; i < daysNumber + 1; ++i) {
     let currDay = moment().subtract(daysNumber - i, 'days').format('YYYY-MM-DD')
     if (data[currDay]) {
       let currInfo = {'in-progress': 0,  'submitted': 0, 'approved': 0, "rejected": 0};
       Object.values(data[currDay]).forEach(item => {
-        if (!currDForm) {
-          currDForm = item.application_name;
-        }
-        if (currDForm === item.application_name) {
+        //if (!currDForm) {
+        //  currDForm = item.application_name;
+        //}
+        //if (currDForm === item.application_name) {
           if (item.application_status === 'unsubmitted') {
             ++currInfo['in-progress'];
           } else {
             ++currInfo[item.application_status];
           }
-        }
+       // }
       });
       Object.keys(pointsY).forEach(item => pointsY[item].push({x: i, y: currInfo[item]}));
     } else {
