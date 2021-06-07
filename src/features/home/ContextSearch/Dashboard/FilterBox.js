90598c91a9f0f715dcf704b9d44dfb0311ce4abc
hryhorchuk117 <hryhorchuk117@gmail.com>
2021-06-07 17:20:06 +0300

    dashboard Dforms format updated

--- src/features/home/ContextSearch/Dashboard/FilterBox.js
+++ src/features/home/ContextSearch/Dashboard/FilterBox.js
@@ -50,7 +50,14 @@ const FilterBox = ({isMap, settings, updateSettings, dForms, setIsFilterBoxOpen}
    useEffect(() => {
     setSelectDForm(settings.dForm);
   }, [settings]);
-  const options = dForms ? dForms.map(item => {return {label: item.d_form.name, value: item.d_form}}) : [];
+
+  const options = [];
+  if (dForms) {
+    Object.keys(dForms).forEach(item => {
+      options.push({label: dForms[item], value: {id: item, name: dForms[item]}})
+    })
+  }
+
   return (
     <Card
       style={styles}
