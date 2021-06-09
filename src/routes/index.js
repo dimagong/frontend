4ba0120090709ef4d5b5409564dda7ae8bb60bc9
hryhorchuk117 <hryhorchuk117@gmail.com>
2021-06-09 16:46:24 +0300

    dashboard removed from the prospects view

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 import routes from "./routes";
 import {v4} from "uuid";
 import {Switch} from "react-router-dom";
 import {PrivateRoute, PublicRoute} from "./RouteProvider";
-import {useSelector} from "react-redux"
+import {useDispatch, useSelector} from "react-redux"
 import {selectAuth} from "app/selectors/authSelectors"
 import {selectProfile} from 'app/selectors'
 import {userService} from 'services/user'
 import {selectUserAbility} from '../app/selectors/userSelectors'
+import {selectContext} from 'app/selectors'
+import appSlice from "../app/slices/appSlice";
+const {setContext} = appSlice.actions;
 
 const Routes = () => {
   const isAuth = useSelector(selectAuth);
   const userRole = useSelector(selectUserAbility);
+  const dispatch = useDispatch()
+  const context = useSelector(selectContext)
 
   const isOnboarding = ["prospect", "adviser"].indexOf(userRole) !== -1;
 
   // if(isAuth && !profile) return null
 
+   useEffect(() => {
+    if (userRole && !isOnboarding && !context) {
+      dispatch(setContext('Dashboard'))
+    }
+  }, [userRole]);
   return (
     <Switch>
       {routes.map((route) => {
