e5ac4f76197e0d855c10ab30711a062b1c811b1a
Han Parker <36171892+HanParkers@users.noreply.github.com>
2021-12-09 23:03:25 +0200

    feat(MF): Added creating master schema fields for member firm.

--- src/api/memberFirms/index.js
+++ src/api/memberFirms/index.js
@@ -15,67 +15,71 @@ import {
   getMemberFirm,
   getMemberFirmActivitiesUrl,
   addFormFieldToMemberFirmUrl,
+  createMasterSchemaFieldForMemberFirmUrl,
 } from "./constants";
 
 const memberFirmsApi = {
   async createMemberFirm({ payload }) {
-    return await requestLayout(createMemberFirm, "POST", payload)
+    return await requestLayout(createMemberFirm, "POST", payload);
   },
 
   async getMemberFirms() {
-    return await requestLayout(getMemberFirms, "GET")
+    return await requestLayout(getMemberFirms, "GET");
   },
 
   async getMemberFirmUsers(memberFirmId) {
-    return await requestLayout(getMemberFirmMembersUrl(memberFirmId), "GET")
+    return await requestLayout(getMemberFirmMembersUrl(memberFirmId), "GET");
   },
 
   async getMemberFirmPotentialUsers(memberFirmId) {
-    return await requestLayout(getPotentialMembers(memberFirmId), "GET")
+    return await requestLayout(getPotentialMembers(memberFirmId), "GET");
   },
 
-  async addMemberFirmUsers({memberFirmId, users}) {
-    return await requestLayout(attachUsersToMemberFirmUrl(memberFirmId), "PUT", {users})
+  async addMemberFirmUsers({ memberFirmId, users }) {
+    return await requestLayout(attachUsersToMemberFirmUrl(memberFirmId), "PUT", { users });
   },
 
-  async removeMemberFirmUsers({memberFirmId, users}) {
-    return await requestLayout(detachUsersFromMemberFirmUrl(memberFirmId), "PUT", {users})
+  async removeMemberFirmUsers({ memberFirmId, users }) {
+    return await requestLayout(detachUsersFromMemberFirmUrl(memberFirmId), "PUT", { users });
   },
 
   async getMasterSchemaFieldsForMemberFirm(memberFirmId) {
-    return await requestLayout(getMasterSchemaFieldsForMemberFirmUrl(memberFirmId), "GET")
+    return await requestLayout(getMasterSchemaFieldsForMemberFirmUrl(memberFirmId), "GET");
   },
 
   async getMemberFirmFormFields(memberFirmId) {
-    return await requestLayout(getMemberFirmFormFieldsUrl(memberFirmId), "GET")
+    return await requestLayout(getMemberFirmFormFieldsUrl(memberFirmId), "GET");
   },
 
   async updateMemberFirmFormValues(payload) {
-    return await requestLayout(updateMemberFirmFormValuesUrl(payload.memberFirmId), "PUT", payload.data)
+    return await requestLayout(updateMemberFirmFormValuesUrl(payload.memberFirmId), "PUT", payload.data);
   },
 
   async updateMemberFirmLogo(payload) {
     // laravel can`t see form data values when method PUT, so
-    payload.logo.append('_method', 'put');
-    return await requestLayout(updateMemberFirmLogo(payload.memberFirmId), "POST", payload.logo)
+    payload.logo.append("_method", "put");
+    return await requestLayout(updateMemberFirmLogo(payload.memberFirmId), "POST", payload.logo);
   },
 
   async removeMemberFirmLogo(payload) {
-    return await requestLayout(removeMemberFirmLogoUrl(payload), "DELETE")
+    return await requestLayout(removeMemberFirmLogoUrl(payload), "DELETE");
   },
 
   async getMemberFirm(payload) {
-    return await requestLayout(getMemberFirm(payload), "GET")
+    return await requestLayout(getMemberFirm(payload), "GET");
   },
 
   async getMemberFirmActivities(payload) {
-    return await requestLayout(getMemberFirmActivitiesUrl(payload.memberFirmId),"GET", {page: payload.page})
+    return await requestLayout(getMemberFirmActivitiesUrl(payload.memberFirmId), "GET", { page: payload.page });
   },
 
-  async addFieldToMemberFirm({payload}) {
-    return await requestLayout(addFormFieldToMemberFirmUrl(payload.memberFirmId), "POST", payload.data)
-  }
+  async addFieldToMemberFirm({ payload }) {
+    return await requestLayout(addFormFieldToMemberFirmUrl(payload.memberFirmId), "POST", payload.data);
+  },
 
+  createMasterSchemaFieldForMemberFirm(payload) {
+    return requestLayout(createMasterSchemaFieldForMemberFirmUrl(payload.member_firm_id), "POST", payload.data);
+  },
 };
 
 export default memberFirmsApi;
