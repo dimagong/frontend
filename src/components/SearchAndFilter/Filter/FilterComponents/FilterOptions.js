68f2af85b26c440b84ff8299533bafb996446f86
hryhorchuk117 <hryhorchuk117@gmail.com>
2022-03-24 15:49:51 +0200

    style(filter): prettify refactoring

--- src/components/SearchAndFilter/Filter/FilterComponents/FilterOptions.js
+++ src/components/SearchAndFilter/Filter/FilterComponents/FilterOptions.js
@@ -1,54 +1,55 @@
-import React from 'react'
-import {
-  Button,
-} from 'reactstrap';
-import {FilterCheckIcon, FilterCrossIcon} from "./FilterAssets";
+import React from "react";
+import { Button } from "reactstrap";
+import { FilterCheckIcon, FilterCrossIcon } from "./FilterAssets";
 import PropTypes from "prop-types";
-import Filter from "../Filter";
 
 const FilterOptions = ({ filter, setFilter, filterOptionsDictionary }) => {
-  const currSelectedFilters = JSON.parse(JSON.stringify(filter)).selectedFilters.find((item, key) => item.key === filter.selectedOptionKey)
+  const currSelectedFilters = JSON.parse(JSON.stringify(filter)).selectedFilters.find(
+    (item, key) => item.key === filter.selectedOptionKey
+  );
 
   const filterOptionIsSelected = (option) => {
-    return !!currSelectedFilters.selected.find((item, key) => item === option)
-  }
+    return !!currSelectedFilters.selected.find((item, key) => item === option);
+  };
 
   const applyFilter = () => {
-    const newSelectedFilters = [...filter.selectedFilters]
-    newSelectedFilters.splice(filter.selectedOptionKey, 1, currSelectedFilters)
-    setFilter({...filter, selectedFilters: newSelectedFilters})
-  }
+    const newSelectedFilters = [...filter.selectedFilters];
+    newSelectedFilters.splice(filter.selectedOptionKey, 1, currSelectedFilters);
+    setFilter({ ...filter, selectedFilters: newSelectedFilters });
+  };
 
   const onSelectFilterOption = (option) => {
     if (filterOptionIsSelected(option)) {
-        currSelectedFilters.selected = currSelectedFilters.selected.filter((item, key) => item !== option)
-      } else {
-        currSelectedFilters.selected.push(option)
-      }
-  }
+      currSelectedFilters.selected = currSelectedFilters.selected.filter((item, key) => item !== option);
+    } else {
+      currSelectedFilters.selected.push(option);
+    }
+  };
 
   const handleFilterSelectCross = (option) => {
     handleFilterSelectCross.selectOptionBySelectingType[currSelectedFilters.settings.addBySelect](option);
-  }
+  };
 
   handleFilterSelectCross.selectOptionBySelectingType = {
     true: (option) => {
-      currSelectedFilters.selected = filterOptionsDictionary[currSelectedFilters.name].filter((item, key) => item !== option)
+      currSelectedFilters.selected = filterOptionsDictionary[currSelectedFilters.name].filter(
+        (item, key) => item !== option
+      );
       currSelectedFilters.settings.addBySelect = false;
       applyFilter();
     },
     false: (option) => {
       onSelectFilterOption(option);
       if (currSelectedFilters.selected.length === filterOptionsDictionary[currSelectedFilters.name].length) {
-        currSelectedFilters.selected = []
+        currSelectedFilters.selected = [];
       }
       applyFilter();
-    }
-  }
+    },
+  };
 
   const handleFilterSelectCheck = (option) => {
     handleFilterSelectCheck.selectOptionBySelectingType[currSelectedFilters.settings.addBySelect](option);
-  }
+  };
 
   handleFilterSelectCheck.selectOptionBySelectingType = {
     true: (option) => {
@@ -57,33 +58,28 @@ const FilterOptions = ({ filter, setFilter, filterOptionsDictionary }) => {
     },
     false: (option) => {
       currSelectedFilters.settings.addBySelect = true;
-      currSelectedFilters.selected = [option]
-      applyFilter()
-    }
-  }
+      currSelectedFilters.selected = [option];
+      applyFilter();
+    },
+  };
 
-  return filterOptionsDictionary[currSelectedFilters.name].map((item, key) =>
-    <Button className={'filter-option not-active'} variant="primary" key={key}>
-      <span className={'filter-name'}>{item}</span>
-      <span className={'filter-right'}>
-          <span>
-            {!filter.crossSelectingDisabled &&
-              <span className={'filter-check'} onClick={() => handleFilterSelectCross(item)}>
-                <FilterCrossIcon
-                  filter={filter}
-                  currOption={item}
-                />
-              </span>
-            }
-            <span className={'filter-check'} onClick={() => handleFilterSelectCheck(item)}>
-              <FilterCheckIcon
-                filter={filter}
-                currOption={item}
-              />
+  return filterOptionsDictionary[currSelectedFilters.name].map((item, key) => (
+    <Button className={"filter-option not-active"} variant="primary" key={key}>
+      <span className={"filter-name"}>{item}</span>
+      <span className={"filter-right"}>
+        <span>
+          {!filter.crossSelectingDisabled && (
+            <span className={"filter-check"} onClick={() => handleFilterSelectCross(item)}>
+              <FilterCrossIcon filter={filter} currOption={item} />
             </span>
+          )}
+          <span className={"filter-check"} onClick={() => handleFilterSelectCheck(item)}>
+            <FilterCheckIcon filter={filter} currOption={item} />
           </span>
         </span>
-    </Button>)
+      </span>
+    </Button>
+  ));
 };
 
 FilterOptions.propTypes = {
--- src/components/SearchAndFilter/Filter/FilterComponents/FilterShorts.js
+++ src/components/SearchAndFilter/Filter/FilterComponents/FilterShorts.js
@@ -1,54 +1,57 @@
-import {Button} from "reactstrap";
+import { Button } from "reactstrap";
 import CloseIcon from "../../../../assets/img/svg/circle-with-cross.svg";
-import React, {useEffect, useState} from "react";
-import {filterOptionsToText} from "../FilterHelper";
-import Filter from "../Filter";
+import React, { useEffect, useState } from "react";
+import { filterOptionsToText } from "../FilterHelper";
 import PropTypes from "prop-types";
 
 const FILTER_DESCRIPTION_SIZE = 40;
 
-const FilterShorts = ({filter, setFilter, objectsToFilter, filterFunction}) => {
+const FilterShorts = ({ filter, setFilter, objectsToFilter, filterFunction }) => {
   const [reloadFilter, setReloadFilter] = useState(false);
 
   const selectedFilters = filter.selectedFilters.filter((item, key) => item.selected.length > 0);
 
   const removeFiltersByTitle = (title) => {
-    const newSelectedFilters = {...filter.selectedFilters.find((item, key) => item.name === title)};
-    newSelectedFilters.selected = []
-    setFilter({...filter, selectedFilters: [...filter.selectedFilters.filter((item, key) => item.name !== title), newSelectedFilters]})
+    const newSelectedFilters = { ...filter.selectedFilters.find((item, key) => item.name === title) };
+    newSelectedFilters.selected = [];
+    setFilter({
+      ...filter,
+      selectedFilters: [...filter.selectedFilters.filter((item, key) => item.name !== title), newSelectedFilters],
+    });
     setReloadFilter(true);
-  }
+  };
 
   useEffect(() => {
     if (reloadFilter) {
-      filterFunction(filter, objectsToFilter)
+      filterFunction(filter, objectsToFilter);
       setReloadFilter(false);
     }
-  }, [reloadFilter])
+  }, [reloadFilter]);
 
   return (
     <div className={`left-orientation-tabs`}>
-      {
-        selectedFilters.map((item, key) => {
-          const outputText = filterOptionsToText(item.selected)
-          return (
-              <Button className={'filter-tab member-firm-filter-tab filter-close-button'} variant={'dark'} key={key}>
-              <span className={'nav-text'}>
-                {outputText.length <= FILTER_DESCRIPTION_SIZE
-                  ? outputText
-                  : `${item.selected.length} ${item.name}`
-                }
-              </span>
+      {selectedFilters.map((item, key) => {
+        const outputText = filterOptionsToText(item.selected);
+        return (
+          <Button className={"filter-tab member-firm-filter-tab filter-close-button"} variant={"dark"} key={key}>
+            <span className={"nav-text"}>
+              {outputText.length <= FILTER_DESCRIPTION_SIZE ? outputText : `${item.selected.length} ${item.name}`}
+            </span>
 
-                <span onClick={() => {removeFiltersByTitle(item.name)}}
-                      className={'close-nav'}><img src={CloseIcon} alt={'close-tab'}/></span>
-              </Button>
-            )
-        })
-      }
-      </div>
-  )
-}
+            <span
+              onClick={() => {
+                removeFiltersByTitle(item.name);
+              }}
+              className={"close-nav"}
+            >
+              <img src={CloseIcon} alt={"close-tab"} />
+            </span>
+          </Button>
+        );
+      })}
+    </div>
+  );
+};
 
 FilterShorts.propTypes = {
   filter: PropTypes.object,
