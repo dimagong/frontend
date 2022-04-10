b14512496ef53eff7fe7f858df1e1de54809e0d3
Han Parker <36171892+HanParkers@users.noreply.github.com>
2022-04-10 12:52:52 +0200

    refactor: Remove query-string package and refactor all related useges.

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,5 +1,3 @@
-import qs from "query-string"
-
 export const loginPath = "/login";
 export const homePath = "/";
 export const forgotPasswordPath = "/forgot-password";
@@ -7,7 +5,7 @@ export const notificationsPath = "/onboarding/notifications";
 export const dformsPath = "/onboarding/dforms";
 export const workflowsPath = "/onboarding/workflows";
 export const userManagmentPath = "/user-management";
-export const userManagmentOptionsPath = id => `${userManagmentPath}?${qs.stringify({user_id: id}, {arrayFormat: 'comma'})}`;
+export const userManagmentOptionsPath = id => `${userManagmentPath}?${new URLSearchParams({ user_id: id })}`;
 export const invitationPath = "/invitation-accept/:invitationId";
 export const onboardingProcessPath = "/onboarding-process";
 export const surveysDesignerExplore = "/surveys/:id";
