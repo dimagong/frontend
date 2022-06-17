884843fd63fac20fc8d9feeb69c59cccb2c2fa68
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2022-06-17 13:24:10 +0300

    fix bug with master schema prop select. Select doesn't clear after selecting another field to edit

--- src/components/FormCreate/Fields/MasterSchemaProperty.js
+++ src/components/FormCreate/Fields/MasterSchemaProperty.js
@@ -164,7 +164,7 @@ export default function MasterSchemaProperty(props) {
         <CustomSelect
           id="select-ms-property"
           options={masterSchemaFields.concat(getAddNewOption())}
-          value={masterSchemaFields.find((next) => parseInt(next.value) === parseInt(props.fieldId))}
+          value={masterSchemaFields.find((next) => parseInt(next.value) === parseInt(props.fieldId)) || null}
           onChange={(event) => {
             if (isObject(event.value) && event.value.isNew) {
               if (!searchableValue) {
