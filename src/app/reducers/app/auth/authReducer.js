85242f12452e223119f733302fb5379f58db2697
Han Parker <36171892+HanParkers@users.noreply.github.com>
2022-07-21 17:48:45 +0200

    refactor(Auth): Made 2FA page, support 2FA to forgot-password page and refactor a bit.

--- src/app/reducers/app/auth/authReducer.js
+++ src/app/reducers/app/auth/authReducer.js
@@ -21,48 +21,9 @@ const logout = (state) => {
   state.isError = null;
 };
 
-const resetPasswordSuccess = (state) => {
-  state.isAuth = false;
-  state.isLoading = false;
-  state.isError = null;
-};
-
-const resetPasswordRequest = (state) => {
-  state.isAuth = false;
-  state.isLoading = true;
-  state.isError = null;
-};
-const resetPasswordError = (state, { payload }) => {
-  state.isAuth = false;
-  state.isLoading = false;
-  state.isError = payload;
-};
-const verifyPasswordSuccess = (state) => {
-  state.isAuth = false;
-  state.isLoading = false;
-  state.isError = null;
-};
-
-const verifyPasswordRequest = (state) => {
-  state.isAuth = false;
-  state.isLoading = true;
-  state.isError = null;
-};
-const verifyPasswordError = (state, { payload }) => {
-  state.isAuth = false;
-  state.isLoading = false;
-  state.isError = payload;
-};
-
 export default {
   loginSuccess,
   loginRequest,
   loginError,
   logout,
-  resetPasswordSuccess,
-  resetPasswordRequest,
-  resetPasswordError,
-  verifyPasswordSuccess,
-  verifyPasswordRequest,
-  verifyPasswordError,
 };
