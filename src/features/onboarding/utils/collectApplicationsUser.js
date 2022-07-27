37aacaf3b367d8184c3f1fe2ee82e711e31491b2
dimagong <dimagong@gmail.com>
2022-07-27 13:29:56 +0200

    fix: Updated collectApplications functionality

--- src/features/onboarding/utils/collectApplicationsUser.js
+++ src/features/onboarding/utils/collectApplicationsUser.js
@@ -2,12 +2,27 @@ import _ from "lodash";
 
 export const collectApplicationsUser = (forms, surveys) => {
   // Onboardings and surveys may have Id collisions, we add tabId property to prevent bugs
-  let userApplications = [...forms, ...surveys].map((application) => {
+  const formsMark = forms.map((application) => {
     return {
       ...application,
-      tabId: `${application.id} ${application.d_form ? "onboarding" : "survey"}`,
+      tabId: `${application.id} onboarding`,
     };
   });
+  const surveyMark = surveys.map((survey) => {
+    return {
+      ...survey,
+      tabId: `${survey.id} survey`,
+    };
+  });
+
+  let userApplications = [...formsMark, ...surveyMark];
+
+  // let userApplications = [...forms, ...surveys].map((application) => {
+  //     return {
+  //         ...application,
+  //         tabId: `${application.id} ${application.d_form ? "onboarding" : "survey"}`,
+  //     };
+  // });
 
   userApplications = _.sortBy(userApplications, (application) => {
     return application.order;
