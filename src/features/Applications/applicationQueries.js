22493287dc0e0400283750943513eb99c7945916
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2022-07-31 14:08:57 +0300

    Feature: create query for application creation

--- src/features/Applications/applicationQueries.js
+++ src/features/Applications/applicationQueries.js
@@ -1,11 +1,14 @@
 import { createQueryKey } from "api/createQueryKey";
 import { useGenericQuery } from "../../api/useGenericQuery";
+import { useGenericMutation } from "../../api/useGenericMutation";
 
 export const AllowedOrganizationsListQuery = createQueryKey("Allowed organizations query");
+export const ApplicationCreateQuery = createQueryKey("Application create query");
 
 export const OrganizationsQueryKeys = {
-  all: () => [AllowedOrganizationsListQuery],
+  all: () => [AllowedOrganizationsListQuery, ApplicationCreateQuery],
   getListById: ({ userId }) => [...AllowedOrganizationsListQuery, { userId }],
+  create: () => [ApplicationCreateQuery],
 };
 
 export const useAllowedOrganizationsListQuery = ({ userId }, options) => {
@@ -15,5 +18,18 @@ export const useAllowedOrganizationsListQuery = ({ userId }, options) => {
       queryKey: OrganizationsQueryKeys.getListById({ userId }),
     },
     options
+  );
+};
+
+export const useApplicationCreateMutation = (options) => {
+  return useGenericMutation(
+    {
+      url: `api/dform-template`,
+      method: "post",
+      queryKey: OrganizationsQueryKeys.create(),
+    },
+    {
+      ...options,
+    }
   );
 };
