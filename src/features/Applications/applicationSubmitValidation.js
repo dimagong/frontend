aa74e157aba260c049ca152ec0c98bfef0931ddb
Han Parker <36171892+HanParkers@users.noreply.github.com>
2022-08-24 17:39:36 +0200

    refactor:(DForm): Styled an applicationSubmitValidation.js.

--- src/features/Applications/applicationSubmitValidation.js
+++ src/features/Applications/applicationSubmitValidation.js
@@ -1,19 +1,18 @@
 import * as yup from "yup";
 
 export const applicationSubmitValidation = yup.object().shape({
-  description: yup.string().required("Each application should have a description"),
-  name: yup.string().required("Each application should have a name"),
-  organization: yup.object().shape({
-    name: yup.string().required("Each organization should have a name"),
-  }),
+  description: yup.string().required("An application should have a description"),
+  name: yup.string().required("An application should have a name"),
+  organization: yup
+    .object()
+    .shape({
+      name: yup.string().required(),
+    })
+    .required("An application should have an organization"),
   fields: yup
     .object()
     .shape({})
-    .test(
-      "Design-mode-validation",
-      "Design mode must contain at least one field", // error message
-      function test(value) {
-        return !!Object.keys(value).length;
-      }
-    ),
+    .test("design-mode-validation", "Design mode must contain at least one field", function test(value) {
+      return Object.keys(value).length > 0;
+    }),
 });
