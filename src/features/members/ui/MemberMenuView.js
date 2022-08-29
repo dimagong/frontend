c11ee16c276e9e1276f2c3d8e2916a7736bfa23c
dimagong <dimagong@gmail.com>
2022-08-29 14:29:34 +0200

    refactor: Updated MemberMenuView

--- src/features/members/ui/MemberMenuView.js
+++ src/features/members/ui/MemberMenuView.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { Row, Col } from "antd";
 import NpmBadge from "./../../nmp-ui/NpmBadge";
+import NpmTooltip from "./../../nmp-ui/NpmTooltip";
+import NpmPopover from "./../../nmp-ui/NpmPopover";
+import { FileProtectOutlined } from "@ant-design/icons";
 
 const MemberMenuView = () => {
+  const [menuOption, onMenuOption] = useState("null");
+
+  const selectMenuOption = (event) => {
+    event.preventDefault();
+    const optionName = event.target.getAttribute("name");
+    onMenuOption(optionName);
+  };
+  console.log("menuOption", menuOption);
+  const content = [
+    { name: "First sirvey", type: "new" },
+    { name: "Second sirvey", type: "pass" },
+  ];
   return (
-    <>
-      <NpmBadge />
-    </>
+    <Row>
+      <Col
+        style={{
+          border: "2px solid blue",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: 200,
+          marginTop: 44,
+        }}
+      >
+        <div style={{ width: "auto" }}>
+          <NpmPopover title={"Applications"} content={content} onClick={selectMenuOption}>
+            <NpmTooltip text="Applications">
+              <NpmBadge name="applications" />
+            </NpmTooltip>
+          </NpmPopover>
+        </div>
+
+        <div style={{ width: "auto" }}>
+          <NpmPopover title={"Surveys"} onClick={selectMenuOption}>
+            <NpmTooltip text="Surveys">
+              <NpmBadge name="survey" icon={<FileProtectOutlined style={{ fontSize: 42, color: "#BCBCBC" }} />} />
+            </NpmTooltip>
+          </NpmPopover>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
