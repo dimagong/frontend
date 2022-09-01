dc725388f42866c05db74f4b21d874792e21a730
dimagong <dimagong@gmail.com>
2022-09-01 16:28:38 +0200

    redesign: Updated custom button

--- src/features/nmp-ui/NpmButton/NpmButton.js
+++ src/features/nmp-ui/NpmButton/NpmButton.js
@@ -5,21 +5,19 @@ import PropTypes from "prop-types";
 import { Button } from "antd";
 
 const NpmButton = (props) => {
-  return (
-    <Button {...props} style={{ backgroundColor: "#22776D" }}>
-      {props.children}
-    </Button>
-  );
+  return <Button {...props}>{props.children}</Button>;
 };
 
 NpmButton.defaultProps = {
   disabled: false,
   size: "middle",
   type: "primary",
+  style: { backgroundColor: "#22776D" },
   onClick: () => {},
 };
 
 NpmButton.propTypes = {
+  style: PropTypes.object,
   disabled: PropTypes.bool,
   size: PropTypes.oneOf(["middle", "large", "small"]),
   type: PropTypes.oneOf(["primary", "ghost", "dashed", "link", "text", "default"]),
