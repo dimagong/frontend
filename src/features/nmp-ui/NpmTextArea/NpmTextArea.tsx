a3700aee00259a4099f0c5013f63a0cddd54ea6d
dimagong <dimagong@gmail.com>
2022-09-12 14:03:52 +0200

    redesign:Updated NpmtextArea

--- src/features/nmp-ui/NpmTextArea/NpmTextArea.tsx
+++ src/features/nmp-ui/NpmTextArea/NpmTextArea.tsx
@@ -6,45 +6,20 @@ import React from "react";
 const { TextArea } = Input;
 
 interface IProps {
-  maxLength: number;
-  placeholder: string;
-  defaultValue: string;
-  value: string | number | readonly string[] | undefined;
-  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  size: "middle" | "small" | "large";
-  addonAfter: React.ReactNode;
-  addonBefore: React.ReactNode;
-  rows: number;
-  style: object;
+  maxLength?: number;
+  placeholder?: string;
+  defaultValue?: string;
+  value?: string | number | readonly string[] | undefined;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  size?: "middle" | "small" | "large";
+  addonAfter?: React.ReactNode;
+  addonBefore?: React.ReactNode;
+  rows?: number;
+  style?: object;
 }
 
-const onChangeDefault = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-  console.log("Change:", e.target.value);
-};
-
-const NpmTextArea = ({
-  maxLength = 200,
-  onChange = onChangeDefault,
-  size = "middle",
-  rows = 6,
-  defaultValue = "",
-  value = "",
-  addonAfter = false,
-  addonBefore = false,
-  style = { height: 120 },
-}: IProps) => {
-  return (
-    <TextArea
-      showCount
-      style={style}
-      onChange={onChange}
-      rows={rows}
-      size={size}
-      maxLength={maxLength}
-      defaultValue={defaultValue}
-      value={value}
-    />
-  );
+const NpmTextArea = (props: IProps) => {
+  return <TextArea {...props} showCount />;
 };
 
 export default NpmTextArea;
