2a8e37d8e60b1ef34f22bc7667e1f928e78ecbcb
dimagong <dimagong@gmail.com>
2022-09-13 18:11:12 +0200

    redesign:Created custom stepper for DForms

---
+++ src/features/nmp-ui/NpmStepper/NpmStepper.tsx
@@ -0,0 +1,47 @@
+import "./styles.scss";
+
+import React from "react";
+
+import { Steps } from "antd";
+
+const { Step } = Steps;
+
+interface IProps {
+  sections: any[];
+  direction?: "vertical" | "horizontal";
+  size?: "small" | "default";
+  current?: number;
+  initial?: number;
+  status?: "wait" | "process" | "finish" | "error";
+  type?: "default" | "navigation";
+  onChange?: (current: number) => void;
+}
+
+const NpmStepper = ({
+  sections = [],
+  direction = "vertical",
+  size = "small",
+  current = 0,
+  initial = 0,
+  status = "process",
+  type = "default",
+  onChange,
+}: IProps) => {
+  return (
+    <Steps
+      direction={direction}
+      size={size}
+      current={current}
+      status={status}
+      initial={initial}
+      type={type}
+      onChange={onChange}
+    >
+      {sections.map((section, idx) => {
+        return <Step key={idx} title={section?.title || ""} description={section?.description || ""} />;
+      })}
+    </Steps>
+  );
+};
+
+export default NpmStepper;
