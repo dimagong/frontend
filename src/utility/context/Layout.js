a572491a00f44f879b7f7fb7bb51f0599d39f93f
Han Parker <36171892+HanParkers@users.noreply.github.com>
2022-11-12 13:54:45 +0100

    fix: Replaced this.props to props in Layout.js

--- src/utility/context/Layout.js
+++ src/utility/context/Layout.js
@@ -3,8 +3,8 @@ import React from "react";
 import FullLayout from "layouts/FullpageLayout";
 import VerticalLayout from "layouts/VerticalLayout";
 
-export const Layout = () => {
-  const { type, children } = this.props;
+export const Layout = (props) => {
+  const { type, children } = props;
   const LayoutComponent = type === "full" ? FullLayout : type === "vertical" ? VerticalLayout : React.Fragment;
 
   return <LayoutComponent>{children}</LayoutComponent>;
