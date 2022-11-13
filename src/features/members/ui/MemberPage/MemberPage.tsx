b6215c0621fe3e5907cb8fe4de11358b9c09a8cc
Han Parker <36171892+HanParkers@users.noreply.github.com>
2022-11-13 13:21:22 +0100

    refactor(member): Started member page markup.

--- src/features/members/ui/MemberPage/MemberPage.tsx
+++ src/features/members/ui/MemberPage/MemberPage.tsx
@@ -1,13 +1,33 @@
-import "assets/styles/new_index.scss";
 import "./styles.scss";
 
+import OrganizationLogo from "./logo.svg";
+
 import React from "react";
+import { Layout } from "antd";
 import type { FC } from "react";
 
+import { NmpCol, NmpRow } from "features/nmp-ui";
+
 export const MemberPage: FC = () => {
+  const organizationName = "";
+
   return (
-    <div>
-      <h1>MemberPage</h1>
-    </div>
+    <Layout className="nmp-member__layout">
+      <Layout.Header prefixCls="nmp-member__header">
+        <NmpRow>
+          <NmpCol span="4">
+            <img src={OrganizationLogo} alt={organizationName} />
+          </NmpCol>
+
+          <NmpCol span="16">Menu</NmpCol>
+
+          <NmpCol span="4">User information</NmpCol>
+        </NmpRow>
+      </Layout.Header>
+
+      <Layout.Content>
+        <h1>MemberPage</h1>
+      </Layout.Content>
+    </Layout>
   );
 };
